a42b279f367098136621a70e8dd9dcb960524492
anishanne <22670462+anishanne@users.noreply.github.com>
2019-06-27 23:23:42 -0400

    Create config.py

---
+++ Argon Source/config.py
@@ -0,0 +1,13 @@
+token="InsertYourTokenHere"
+bprefix="InsertYourPrefixHere"
+mongodb="InsertYourMongoDBUrlHere"
+
+#Codes For Icons (ex. <:settings:592900202445144076>)
+scheck="EnterCheckEmojiCode" 
+sx="EnterXEmojiCodeHere"
+ssettings="EnterSettingsEmojiCode"
+senabled="EnterEnabledEmojiCode"
+sdisabled="EnterDisabledEmojiCode"
+
+#Default Bot Embed
+sembed=0x7289da
